9e625997c73d730f1c39e4f6347438da70ff3523
ValiantsinaP <vpauliuchenkava@gmail.com>
2023-04-24 16:32:27 -0600

    SantaInTheCity
    SantaInTheCity

---
+++ script.js
@@ -0,0 +1,40 @@
+function christmasCountdown (){
+    const christmasDate = new Date("December 25, 2023 00:00");
+    const now = new Date();
+    const diff = christmasDate - now;
+    
+    const msInSecond = 1000;
+    const msInMinute = 60 * 1000;
+    const msInHour = 60 * 60 * 1000;
+    const msInDay = 24 * 60 * 60 * 1000;
+
+    const displayDay = Math.floor(diff/msInDay);
+    document.querySelector(".days").textContent = displayDay;
+    const displayHours = Math.floor((diff%msInDay) / msInHour);
+    document.querySelector(".hours").textContent = displayHours;
+    const displayMinutes = Math.floor((diff%msInHour) / msInMinute);
+    document.querySelector(".minutes").textContent = displayMinutes;
+    const displaySeconds = Math.floor((diff%msInMinute) / msInSecond);
+    document.querySelector(".seconds").textContent = displaySeconds;
+
+    if (diff <=0) {
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timeID);
+        merryChristmas()
+}
+}
+
+let timeID = setInterval(christmasCountdown, 1000);
+
+function merryChristmas(){
+    const heading = document.querySelector("h1");
+    heading.textContent = "MERRY CHRISTMAS!!! HO-HO-HO!!!";
+    heading.classList.add("red");
+}
+const button = document.querySelector("#myButton");
+button.addEventListener('click', function(){
+    document.querySelector("#myAudio").play();
+})
